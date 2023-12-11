0ae9fbe60f89d63c29854d1dc3c639c5828d17ea
Hin <tonghohin77@gmail.com>
2023-12-11 15:09:03 -0400

    Kafka

--- src/main/java/com/shop/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/shop/notificationservice/NotificationServiceApplication.java
@@ -2,12 +2,23 @@ package com.shop.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.shop.notificationservice.event.OrderPlacedEvent;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@KafkaListener(topics = "notificationTopic")
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+		log.info("Notification is sent to the user for the order number {}", orderPlacedEvent.getOrderNumber());
 	}
 
 }
---
+++ src/main/java/com/shop/notificationservice/event/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.shop.notificationservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderNumber;
+
+}
